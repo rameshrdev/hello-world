271fd236287c77e50a8d7126d4087d04ceb1c481
rameshrdev <rmshrose@gmail.com>
2016-12-16 07:38:00 +0530

    test

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 <?php 
 $jsonArray = array();
-$allName = array('Ramesh', 'Ramesh', 'Ramesh', 'Ramesh', 'Ramesh', 'Ramesh');
+$allName = array('Ramesh', 'Ramesh', 'Ramesh', 'Ramesh', 'Ramesh', 'Rameshsss');
 $allGame = array('Cricket', 'Football', 'Basketball', 'Table Tennis', 'Hockey', 'Kabadi');
 foreach($allName as $key=>$name){
 	$jsonArray[] = array('name'=>$name, 'game'=>$allGame[$key]);
